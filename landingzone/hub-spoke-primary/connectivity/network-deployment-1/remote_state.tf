db880fc09c07bd24cb17ac57ea55bf33c2490cbb
bryanlong1977-ai <bryan.m.long@gmail.com>
2026-02-18 10:05:40 -0500

    Delete landingzone/hub-spoke-primary directory

--- landingzone/hub-spoke-primary/connectivity/network-deployment-1/remote_state.tf
+++
@@ -1,3 +0,0 @@
-# No remote state dependencies for Network Deployment 1
-# This is the first deployment in the connectivity subscription
-# Subsequent deployments will reference outputs from this deployment
